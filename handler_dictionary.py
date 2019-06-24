87bccf6c7767e22267134849e1fb068398ad98ae
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-24 21:24:36 +0300

    Create handler_dictionary.py

---
+++ handler_dictionary.py
@@ -0,0 +1,15 @@
+def get_dict_with_questions_and_answers(path, encoding):
+    question_dict = {}
+    file_with_information = open(path, "r", encoding=encoding).read().split('\n\n\n')
+    for part in file_with_information:
+        question_with_answer = part.split('\n\n')
+        for text in question_with_answer:
+            if 'Вопрос' in text:
+                question = re.split(r':', text ,maxsplit=1)
+                text_question = question[1].replace('\n', '')
+            if 'Ответ' in text:
+                answer = re.split(r':', text ,maxsplit=1)
+                text_answer = answer[1].replace('\n', '')
+        question_dict[text_question] = text_answer    
+    
+    return question_dict
