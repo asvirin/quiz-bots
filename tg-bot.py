fa1767a111afd9cefae1eacf7524f9a04450297e
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-25 23:16:46 +0300

    Update tg-bot.py

--- tg-bot.py
+++ tg-bot.py
@@ -81,8 +81,8 @@ if __name__ == '__main__':
             SURRENDER: [RegexHandler('^Сдаться$', handle_loss, r)],
 
             CHECK_ANSWER: 
-            [RegexHandler('^Новый вопрос$', handle_new_question_request)],
-            [MessageHandler(Filters.text, handle_solution_attempt, r)]
+            [RegexHandler('^Новый вопрос$', handle_new_question_request),
+                MessageHandler(Filters.text, handle_solution_attempt, r)]
         },
 
         fallbacks=[CommandHandler('cancel', cancel)])
